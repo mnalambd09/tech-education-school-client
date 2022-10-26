e52ec5de768241525975fb3bdfbb5a5a4367a95c
mnalambd09 <mnalambd09@gmail.com>
2022-10-26 11:13:19 +0600

    logo and route has been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './Components/Home/Home';
+import Blog from './Components/Blog/Blog';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
 
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children:[
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+      ]
+    },
+  ])
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from '../Components/Header/Header';
 import './Main.css'
 
 const Main = () => {
     return (
         <div>
-            <h1>This is Main</h1>
+            <Header></Header>
+            <Outlet></Outlet>
         </div>
     );
 };
