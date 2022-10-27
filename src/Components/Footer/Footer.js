b2a2307741968c2468b2f7116b4bcce0dc6e5a27
mnalambd09 <mnalambd09@gmail.com>
2022-10-27 19:59:25 +0600

    footer section added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
+import { FaFacebook, FaGithub, FaGoogle, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div className='bg-primary p-5 text-white text-center'>
-            <h1>This is Footer</h1>
+        <div className='bg-black p-5 text-white text-center'>
+            <div>
+                <div><h4>All Right Reserved</h4></div>
+                <div className='social-icon'>
+                    <h5>Follow us on Social Network</h5>
+                <FaGoogle />
+                <FaGithub />
+                <FaFacebook></FaFacebook>
+                <FaTwitter></FaTwitter>
+                <FaLinkedin></FaLinkedin>
+                </div>
+            </div>
         </div>
     );
 };
